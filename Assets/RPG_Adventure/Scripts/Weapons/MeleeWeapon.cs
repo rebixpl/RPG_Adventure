7892ddcdeade86b57407cd05468345e07371c356
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2022-03-06 11:48:04 +0100

    sword raycasting

--- Assets/RPG_Adventure/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/RPG_Adventure/Scripts/Weapons/MeleeWeapon.cs
@@ -15,9 +15,40 @@ namespace RPG_Adventure
         public int damage = 10;
         public AttackPoint[] attackPoints = new AttackPoint[0];
 
+        private bool m_IsAttack = false;
+        private Vector3[] m_OriginAttackPos;
+
+        private void FixedUpdate()
+        {
+            if (m_IsAttack)
+            {
+                for (int i = 0; i < attackPoints.Length; i++)
+                {
+                    AttackPoint ap = attackPoints[i];
+                    Vector3 worldPos = ap.rootTransform.position
+                        + ap.rootTransform.TransformDirection(ap.offset);
+
+                    Vector3 attackVector = worldPos - m_OriginAttackPos[i];
+
+                    // Draw a ray
+                    Ray r = new Ray(worldPos, attackVector);
+                    Debug.DrawRay(worldPos, attackVector, Color.red, 4.0f);
+                }
+            }
+        }
+
         public void BeginAttack()
         {
-            Debug.Log("Weapon is attacing");
+            m_IsAttack = true;
+            m_OriginAttackPos = new Vector3[attackPoints.Length];
+
+            // Save original positions of attack points before sword swings
+            for (int i = 0; i < attackPoints.Length; i++)
+            {
+                AttackPoint ap = attackPoints[i];
+                m_OriginAttackPos[i] = ap.rootTransform.position +
+                    ap.rootTransform.TransformDirection(ap.offset);
+            }
         }
 
 #if UNITY_EDITOR
